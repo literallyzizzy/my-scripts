8e5a9df88dbc65e37209942b8d17ee4504de4e97
Zizzy <166898312+literallyzizzy@users.noreply.github.com>
2024-07-21 14:43:03 +0100

    Create temp.lua

---
+++ temp.lua
@@ -0,0 +1,14 @@
+--[[
+░█▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ █▀▀ █▀▀ 
+░█▄▄█ █──█ ▀▀█ ▀▀█ █▀▀ ▀▀█ ▀▀█ 
+░█─── ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀
+░█▀▀▀ █──█ █▀▀ █─█ █▀▀ █▀▀█ 
+░█▀▀▀ █──█ █── █▀▄ █▀▀ █▄▄▀ 
+░█─── ─▀▀▀ ▀▀▀ ▀─▀ ▀▀▀ ▀─▀▀
+]]
+
+
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Possess Fucker", HidePremium = true, SaveConfig = true, ConfigFolder = "OrionTest"})
