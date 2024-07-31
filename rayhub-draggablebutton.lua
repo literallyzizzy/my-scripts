2acb2c6b075ffc0b2501c7a5925e8ad4ce9cf515
Zizzy <166898312+literallyzizzy@users.noreply.github.com>
2024-07-31 15:31:39 +0100

    Update rayhub-draggablebutton.lua

--- rayhub-draggablebutton.lua
+++ rayhub-draggablebutton.lua
@@ -1,74 +1,74 @@
 -- Instances:
 
 local rayhub = game:GetService("CoreGui"):WaitForChild("RayHub")
-local RayHubToggle = Instance.new("ScreenGui")
-local ToggleButton = Instance.new("ImageButton")
+local DraggableButton = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
 local UICorner = Instance.new("UICorner")
 
 --Properties:
 
-RayHubToggle.Name = "RayHubToggle"
-RayHubToggle.Parent = CoreGui
+DraggableButton.Name = "DraggableButton"
+DraggableButton.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+DraggableButton.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-ToggleButton.Name = "ToggleButton"
-ToggleButton.Parent = RayHubToggle
-ToggleButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ToggleButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ToggleButton.BorderSizePixel = 0
-ToggleButton.Position = UDim2.new(0.502141595, 0, 0.122569606, 0)
-ToggleButton.Size = UDim2.new(0, 49, 0, 49)
-ToggleButton.Image = "rbxassetid://18726080466"
+ImageButton.Parent = DraggableButton
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.500625253, 0, 0.121980675, 0)
+ImageButton.Size = UDim2.new(0, 54, 0, 54)
+ImageButton.Image = "rbxassetid://18726080466"
 
 UICorner.CornerRadius = UDim.new(0, 10)
-UICorner.Parent = ToggleButton
+UICorner.Parent = ImageButton
 
 -- Scripts:
 
-local function ZUGHZR_fake_script() -- ToggleButton.Dragify
-local script = Instance.new('LocalScript', ToggleButton)
+local function MUBRB_fake_script() -- ImageButton.Dragify 
+	local script = Instance.new('LocalScript', ImageButton)
 
-local UserInputService = game:GetService("UserInputService")
-
-local gui = script.Parent
-
-local dragging
-local dragInput
-local dragStart
-local startPos
-
-local function update(input)
-local delta = input.Position - dragStart
-gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
 end
+coroutine.wrap(MUBRB_fake_script)()
 
-gui.InputBegan:Connect(function(input)
-if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-dragging = true
-dragStart = input.Position
-startPos = gui.Position
-
-input.Changed:Connect(function()
-if input.UserInputState == Enum.UserInputState.End then
-dragging = false
-end
-end)
-end
-end)
-
-gui.InputChanged:Connect(function(input)
-if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-dragInput = input
-end
-end)
-
-UserInputService.InputChanged:Connect(function(input)
-if input == dragInput and dragging then
-update(input)
-end
-end)
-end
-coroutine.wrap(ZUGHZR_fake_script)()
-
-ToggleButton.MouseButton1Click:Connect(function()
+ImageButton.MouseButton1Click:Connect(function()
 rayhub.Enabled = not rayhub.Enabled
 end)
